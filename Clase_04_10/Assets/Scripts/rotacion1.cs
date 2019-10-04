f7fdcb8aedb4b3c9f2cce4d4626f8ab6e1f4e23e
Guillermo Vera <vfguille@gmail.com>
2019-10-04 11:48:15 +0200

    Clase 04-10-19

---
+++ Clase_04_10/Assets/Scripts/rotacion1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotacion1 : MonoBehaviour
+{
+    public GameObject _objetoDestino;
+    public float _vel = 0.01F;
+    private float mitades;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        mitades = _objetoDestino.transform.localScale.z / 2 + transform.localScale.z / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            var position = transform.position;
+            var position1 = _objetoDestino.transform.position;
+            position = Vector3.MoveTowards(position, new Vector3(position1.x,
+                position.y, position1.z + mitades), _vel);
+            transform.position = position;
+        }
+    }
+}
---
+++ Clase_04_10/Assets/Scripts/rotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    private Quaternion _origen;
+    private Quaternion _destino;
+    public float _vel = 0F;
+
+    // Use this for initialization
+    void Start()
+    {
+        _origen = transform.rotation;
+        _destino = Quaternion.Euler(0, 90, 0);
+        _vel *= Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _vel = Time.time * 0.1F;
+        _origen = transform.rotation;
+        //if (Input.GetKeyDown(KeyCode.Z))
+        transform.rotation = Quaternion.Lerp(_origen, _destino, _vel);
+
+        
+        Debug.Log("Vel: " + _vel);
+    }
+}
