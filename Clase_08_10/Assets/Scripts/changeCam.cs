275ccdb3dd96c995baff0ee162dcbf2da05f01cd
Guillermo Vera <vfguille@gmail.com>
2019-10-11 11:45:46 +0200

    Clase 11-10-19

---
+++ Clase_08_10/Assets/Scripts/changeCam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeCam : MonoBehaviour
+{
+
+	public GameObject cam1;
+	public GameObject cam2;
+ 
+	 void Start() {
+		cam1.SetActive(true); 
+		cam2.SetActive(false);
+	 }
+
+	void Update()
+	{
+
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			cam1.active = !cam1.active;
+			cam2.active = !cam2.active;
+		}
+	}
+}
