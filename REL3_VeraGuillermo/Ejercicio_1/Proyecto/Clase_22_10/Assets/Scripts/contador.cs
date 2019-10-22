ff2524fb40fc80ac821c2fd295062352e4d4ae77
Guillermo Vera <vfguille@gmail.com>
2019-10-22 14:41:05 +0200

    Clase 22-10-19

---
+++ REL3_VeraGuillermo/Ejercicio_1/Proyecto/Clase_22_10/Assets/Scripts/contador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class contador : MonoBehaviour {
+public Text _contador;
+private double _myTime = 0;
+private float _vel = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_myTime += Time.deltaTime * Math.Exp((double)_vel);
+		_contador.text = _myTime.ToString("0000");
+
+	}
+
+	public void poner0()
+	{
+		_contador.text = "0";
+	}
+}
