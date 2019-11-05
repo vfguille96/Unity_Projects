dbdf23e8067d475757425e4cd45d1eb3b045c5f5
Guillermo Vera <vfguille@gmail.com>
2019-11-05 14:40:27 +0100

    Clase 05_11

---
+++ CLASE/ControlCombustible/Assets/Scripts/GasControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GasControl : MonoBehaviour
+{
+	public static bool _Gas = true;
+	private float _incr = 10;
+	public Slider _slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Z to incr. gas
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			_slider.value += _incr;
+			_Gas = true;
+		}
+		
+		// Decr. gas X
+		if (Input.GetKeyDown(KeyCode.X))
+		{
+			_slider.value -= _incr;
+			_Gas = true;
+			if (_slider.value <= 0)
+				_Gas = false;
+		}
+	}
+}
---
+++ CLASE/ControlCombustible/Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	private const float _MIN = -5;
+	private const float _MAX = 5;
+	private float _posX;
+	private float _posZ;
+	private float _vel = 7;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_posX = 0;
+		_posZ = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (GasControl._Gas)
+		{
+			_posX += Input.GetAxis("Horizontal") * _vel * Time.deltaTime;
+			_posZ += Input.GetAxis("Vertical") * _vel * Time.deltaTime;
+			_posX = Mathf.Clamp(_posX, _MIN, _MAX);
+			_posZ = Mathf.Clamp(_posZ, _MIN, _MAX);
+			transform.position = new Vector3(_posX, transform.position.y, _posZ);
+		}
+	}
+}
