4b70cbe20a9a7c74b399e7b84edb117e04c8a027
Guillermo Vera <vfguille@gmail.com>
2019-11-14 01:26:38 +0100

    Escenas REL3_EJ1

---
+++ RELACIONES/REL3_VeraGuillermo/Ejercicio_1/Proyecto/REL3_EJ1/Assets/Scripts/GoToScene.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoToScene : MonoBehaviour
+{
+
+	public Button button;
+	
+	// Use this for initialization
+	void Start () {
+		button.onClick.AddListener(ChangeScene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Escape))
+			QuitGame();
+	}
+
+	public void ChangeScene()
+	{
+		switch (button.name)
+		{
+			case "btEsc1":
+				SceneManager.LoadScene("Menu1");
+				break;
+			case "btEsc2":
+				SceneManager.LoadScene("Menu2");
+				break;
+			case "btEsc3":
+				SceneManager.LoadScene("Menu3");
+				break;
+			case "btEsc4":
+				SceneManager.LoadScene("Menu4");
+				break;
+			case "btEscape":
+				QuitGame();
+				break;
+		}
+	}
+
+	public void QuitGame() { Application.Quit(); }
+}
