36995858ad07afc8643aa5342ed43d5f4b9a2ee6
Guillermo Vera <vfguille@gmail.com>
2019-11-22 12:48:30 +0100

    Clase 22_11

---
+++ CLASE/CinematicasYFisicas/Assets/Scripts/Mover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+	/**
+	 * Hay 4 tipos de fuerzas, AddForce():
+	 * 	1- Force: se aplica una fuerza continua, teniendo en cuenta la masa de objeto.
+	 * 	2- Acceleration: aplica una fuerza continua, ignorando la masa.
+	 * 	3- Impulse: aplica una fuerza instantánea, teniendo en cuenta la masa.
+	 * 	4- VelociteChanged: aplica una fuerza instantánea, ignorando la masa.
+	 */
+	// Use this for initialization
+
+	public float _vel = 50F;	// Debe ser más elevado, ya que con FixedUpdate() se ejecuta menos veces por segundo que en Update().
+	private Rigidbody _rigidbody;
+	private Vector3 _fuerzaEmpuje;
+	void Start ()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		_fuerzaEmpuje = new Vector3(Input.GetAxis("Horizontal") * _vel * Time.deltaTime, 0, Input.GetAxis("Vertical") * _vel * Time.deltaTime);
+		_rigidbody.AddForce(_fuerzaEmpuje);
+		//_rigidbody.freezeRotation = true;	 // Hace que se congele la rotación. (En veh de roá', es resbalá).
+	}
+}
---
+++ CLASE/CinematicasYFisicas/Assets/Scripts/Trasladar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Trasladar : MonoBehaviour
+{
+	public TextMeshPro _magnitud;
+
+	public Transform _origen;
+	public Transform _destino;
+
+	private float _vel;
+
+	private Vector3 _distancia;
+	private Vector3 _direccion;
+
+	private bool _enMovimiento;
+	// Use this for initialization
+	void Start ()
+	{
+		_vel = 0.01F;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		DetectarMovimiento();
+		Trasladando();
+	}
+
+	private void DetectarMovimiento()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			_enMovimiento = true;
+	}
+
+	void Trasladando()
+	{
+		_distancia = _destino.position - _origen.position;
+		if (_enMovimiento)
+		{
+			_direccion = _destino.position - _origen.position;
+			_origen.Translate(_direccion * _vel);
+		}
+
+		_magnitud.text = _distancia.magnitude.ToString();
+		if (_distancia.magnitude < 0.2F)
+			_enMovimiento = false;
+	}
+}
