8d839cc65c13a49f512377f3cf8d00f1961bb5ed
Guillermo Vera <vfguille@gmail.com>
2019-11-23 20:26:50 +0100

    REL3_EJ4 terminado

---
+++ RELACIONES/REL3_VeraGuillermo/Ejercicio_4/Proyecto/REL3_EJ4/Assets/Scripts/CalculateScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEngine;
+
+public class CalculateScore : MonoBehaviour {
+
+	// Use this for initialization
+	private string path = @"score.txt";
+	private TextMeshProUGUI _tmScore;
+	void Start ()
+	{
+		_tmScore = gameObject.GetComponent<TextMeshProUGUI>();
+		GenerateAndSetScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	private void GenerateAndSetScore()
+	{
+		float score = Random.Range(100, 99999);
+		_tmScore.text = score.ToString();
+		
+			string scoreToWrite = score + ";";
+			if (!File.Exists(path))
+				File.WriteAllText(path, scoreToWrite);
+			else
+				File.AppendAllText(path, scoreToWrite);
+	}
+}
---
+++ RELACIONES/REL3_VeraGuillermo/Ejercicio_4/Proyecto/REL3_EJ4/Assets/Scripts/SaveNamePlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveNamePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	private GameObject _button;
+	private Button _buttonSave;
+	public InputField _inputF;
+	public Canvas _canvasName;
+	public Canvas _canvasScore;
+	private GameObject _canvasso;
+	private string path = @"score.txt";
+	void Start ()
+	{
+		_canvasScore.enabled = false;
+		_canvasso = GameObject.Find("ImageScore");
+		_canvasso.SetActive(false);
+		_button = GameObject.Find("BtSaveNamePlayer");
+		_button.GetComponent<Button>().onClick.AddListener(SaveNamePlayerFile);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void HideCanvasName()
+	{
+		_canvasName.enabled = false;
+		_canvasScore.enabled = true;
+		_canvasso.SetActive(true);
+	}
+
+	void SaveNamePlayerFile()
+	{
+		string namePlayer = _inputF.text + ";";
+		if (!File.Exists(path))
+			File.WriteAllText(path, namePlayer);
+		else
+			File.AppendAllText(path, namePlayer);
+		
+		HideCanvasName();
+	}
+}
