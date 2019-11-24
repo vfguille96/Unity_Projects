b082e9665caf2b451df76c81ea0ce2a7b9a4da10
Guillermo Vera <vfguille@gmail.com>
2019-11-24 17:47:22 +0100

    REL3_EJ6 terminado

---
+++ RELACIONES/REL3_VeraGuillermo/Ejercicio_6/Proyecto/Assets/Scripts/ControlBotones.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlBotones : MonoBehaviour
+{
+	private float _vel = 0.8F;	// Mov. XY
+	private float _velZoom = 0.008F;
+	
+	// Movimientos
+	private bool up = false;
+	private bool down = false;
+	private bool rigth = false;
+	private bool left = false;
+	private bool zoomIn = false;
+	private bool zoomOut = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rigth)
+			transform.Translate((Vector3.right * Time.deltaTime * _vel));
+		if (left)
+			transform.Translate((Vector3.left * Time.deltaTime * _vel));
+		if (up)
+			transform.Translate((Vector3.up * Time.deltaTime * _vel));
+		if (down)
+			transform.Translate((Vector3.down * Time.deltaTime * _vel));
+		
+		if (zoomIn)
+			transform.localScale = new Vector3(transform.localScale.x + _velZoom, 
+											transform.localScale.y + _velZoom, 
+											transform.localScale.z + _velZoom);
+		if (zoomOut)
+			transform.localScale = new Vector3(transform.localScale.x - _velZoom, 
+											transform.localScale.y - _velZoom, 
+											transform.localScale.z - _velZoom);
+	}
+	
+	// Métodos control mov.
+	public void MoveRight() { rigth = true; }
+	public void MoveLeft() { left = true; }
+	public void MoveUp() { up = true; }
+	public void MoveDown() { down = true; }
+
+	public void Stop()
+	{
+		up = false;
+		down = false;
+		rigth = false;
+		left = false;
+		zoomIn = false;
+		zoomOut = false;
+	}
+
+	// Métodos control zoom
+	public void ZommIn() { zoomIn = true; }
+	public void ZoomOut() { zoomOut = true; }
+}
---
+++ RELACIONES/REL3_VeraGuillermo/Ejercicio_6/Proyecto/Assets/Scripts/Rotando.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotando : MonoBehaviour
+{
+	private Vector3 _velRot;
+	// Use this for initialization
+	void Start () {
+		_velRot = new Vector3(0.53F, 0.53F, 0.53F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(_velRot);
+	}
+}
