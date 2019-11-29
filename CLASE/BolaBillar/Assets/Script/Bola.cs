082042dd6fc68849322089821e8d7752d0be4668
Guillermo Vera <vfguille@gmail.com>
2019-11-29 12:45:18 +0100

    Clase 29_11

---
+++ CLASE/BolaBillar/Assets/Script/Bola.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+	public float _fuerzaTorque = 300;
+	private Vector3 _aplicarFuerza;
+	public float _fuerzaImpulso = 300;
+	private Rigidbody _rigidbody;
+
+	void Start ()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_aplicarFuerza = - Time.deltaTime * _fuerzaTorque * Input.GetAxis("Horizontal") * Vector3.up;
+		_rigidbody.AddTorque(_aplicarFuerza, ForceMode.Force);
+		if (Input.GetButtonDown("Vertical"))
+		{
+			 GetComponent<Rigidbody>().AddForce(Time.deltaTime * _fuerzaImpulso * Vector3.forward);
+		}
+	}
+}
---
+++ CLASE/BolaBillar/Assets/Script/Cubo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Cubo : MonoBehaviour
+{
+	[FormerlySerializedAs("_Vector3")] public Vector3 _posInicial;
+	private Quaternion q1;
+
+	private Rigidbody _rigidbody;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+		_posInicial = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		_rigidbody.position = _posInicial;
+	}
+}
