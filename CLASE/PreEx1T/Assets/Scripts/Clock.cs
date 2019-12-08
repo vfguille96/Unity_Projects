cae34932ce6ddd18a085ee47e4b0ad4a72e5e667
Guillermo Vera <vfguille@gmail.com>
2019-12-08 23:08:30 +0100

    EJ3 empezado

--- CLASE/PreEx1T/Assets/Scripts/Clock.cs
+++ CLASE/PreEx1T/Assets/Scripts/Clock.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Clock : MonoBehaviour
@@ -10,15 +11,20 @@ public class Clock : MonoBehaviour
     public GameObject _hour;
     public GameObject _minute;
     public Slider _slider;
+    
+    private const float _consH = 360F / 60F;
+    private const float _consM = 360F / 5F;
 
     void Start()
     {
+        _minute.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        _hour.transform.localRotation = Quaternion.Euler(0, 0, 0);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        _minute.transform.localRotation = Quaternion.Euler(0, 0, _slider.value * Time.time * -(360 / 60) * 12);
-        _hour.transform.localRotation = Quaternion.Euler(0, 0, _slider.value * Time.time * -(360F / 120));
+        _minute.transform.localRotation = Quaternion.Euler(0, 0, _slider.value * Time.time * -_consM);
+        _hour.transform.localRotation = Quaternion.Euler(0, 0, _slider.value * Time.time * -_consH);
     }
 }
---
+++ CLASE/PreEx1T/Assets/Scripts/CountDown.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour {
+
+	// Use this for initialization
+	public Slider _Slider;
+	public InputField _InputH;
+	public InputField _InputM;
+	public InputField _InputS;
+	public Button _Button;
+	private DateTime _dateTime;
+	public Text _Error;
+
+	void Start ()
+	{
+		_Button.GetComponent<Text>().text = "Marcha";
+		_Button.onClick.AddListener(Pressed);
+		_Error.color = Color.black;
+		_Slider.value = 0;
+	}
+
+	private void Pressed()
+	{
+		if (_Button.name == "Button")
+		{
+			try
+			{
+				_dateTime = Convert.ToDateTime(DateTime.Parse(_InputH.text + ":" + _InputM.text + ":" + _InputS.text).ToShortTimeString());
+			}
+			catch (Exception e)
+			{
+				_Error.color = Color.red;
+				_dateTime = DateTime.Parse("00:00:00");
+				return;
+			}
+
+			_Button.GetComponent<Text>().text = "Paro";
+			
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
