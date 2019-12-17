6f31e213ac1f87fd72fd35b67e36989ece7f40b3
Guillermo Vera <vfguille@gmail.com>
2019-12-17 14:40:06 +0100

    Clase 17_12

---
+++ CLASE/Rotaciones/Assets/AvionEuler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvionEuler : MonoBehaviour
+{
+	public float _velRot = 90;
+
+	// Los miembros se heredan.
+	protected float _rotEjeX;
+	protected float _rotEjeY;
+	protected float _rotEjeZ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Asignar los valores en función de la tecla pulsada.
+		_rotEjeZ = Input.GetAxis("Vertical");
+		_rotEjeY = Input.GetAxis("Horizontal");
+		// Se usan las teclas Z y X
+		_rotEjeX = Input.GetAxis("EjeX");
+		
+		AplicarRotacion();
+	}
+
+	protected virtual void AplicarRotacion()
+	{
+		Vector3 rotacionActual = transform.localEulerAngles;
+		
+		// Modificamos la rotación actual con los valores leídos.
+		rotacionActual.x += _rotEjeX * _velRot * Time.deltaTime;
+		rotacionActual.y += _rotEjeY * _velRot * Time.deltaTime;
+		rotacionActual.z += _rotEjeZ * _velRot * Time.deltaTime;
+
+		transform.eulerAngles = rotacionActual;
+	}
+
+	private void OnGUI()
+	{
+		GUI.color = Color.black;
+		GUILayout.Label("Rotacion X: " + _rotEjeX);
+		GUILayout.Label("Rotacion Y: " + _rotEjeY);
+		GUILayout.Label("Rotacion Z: " + _rotEjeZ);
+	}
+}
---
+++ CLASE/Rotaciones/Assets/AvionQuaternion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvionQuaternion : AvionEuler {
+
+	
+
+	protected override void AplicarRotacion()
+	{
+		Vector3 _rotActual = Vector3.zero;
+		_rotActual.x = _rotEjeX;
+		_rotActual.y = _rotEjeY;
+		_rotActual.z = _rotEjeZ;
+
+		Quaternion rotacion = Quaternion.Euler(_rotActual * (_velRot * Time.deltaTime));
+
+		transform.rotation *= rotacion;
+	}
+}
