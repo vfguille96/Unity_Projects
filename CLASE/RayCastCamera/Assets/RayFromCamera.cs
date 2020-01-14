890e95a507e23c882ae587eac515f93b9e17f9b7
Guillermo Vera <vfguille@gmail.com>
2020-01-14 14:43:03 +0100

    Clase 14_01

---
+++ CLASE/RayCastCamera/Assets/RayFromCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayFromCamera : MonoBehaviour
+{
+	private Ray _ray;
+	private RaycastHit _raycastHit;
+	private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+		_ray = new Ray();
+		_camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			_ray = _camera.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(_ray, out _raycastHit))
+			{
+				Debug.LogFormat("Detectado objeto {0}", _raycastHit.collider.name);
+
+				GameObject o;
+				(o = _raycastHit.collider.gameObject).GetComponent<Rigidbody>().useGravity = true;
+				Destroy(o, 0.8F);
+				
+			}
+		}
+	}
+}
---
+++ CLASE/RayCastCamera/Assets/SphereCast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCast : MonoBehaviour
+{
+	private Vector3 _posI;
+	private float _radSphere;
+	private RaycastHit _infoRay;
+	private Vector3 _dirRay;
+	private float _longRay;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_radSphere = transform.localScale.x / 2;
+		_dirRay = Vector3.forward;
+		_longRay = 100;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_posI = transform.position;
+		if (Physics.SphereCast(_posI, _radSphere, _dirRay, out _infoRay, _longRay))
+		{
+			Debug.LogFormat("Detectado un obstáculo: {0} a {1} metros.", _infoRay.collider.name, _infoRay.distance);
+			_infoRay.collider.gameObject.GetComponent<Renderer>().material.color = Color.green;
+		}
+
+	}
+}
