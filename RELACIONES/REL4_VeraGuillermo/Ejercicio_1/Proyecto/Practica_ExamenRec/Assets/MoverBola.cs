5a54c44a856b67a3bc83e2b387f6f8f24f5a8940
Guillermo Vera <vfguille@gmail.com>
2020-01-17 12:41:53 +0100

    Clase 17_01

---
+++ RELACIONES/REL4_VeraGuillermo/Ejercicio_1/Proyecto/Practica_ExamenRec/Assets/MoverBola.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverBola : MonoBehaviour {
+
+	private const float VEL = 20;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * VEL, 0, Input.GetAxis("Vertical") * Time.deltaTime * VEL);
+	}
+}
