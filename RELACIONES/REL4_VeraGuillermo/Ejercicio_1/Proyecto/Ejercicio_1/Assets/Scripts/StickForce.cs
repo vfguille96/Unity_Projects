4785138ec0fcfedb4e86422ad008f70c9cc06c8f
Guillermo Vera <vfguille@gmail.com>
2020-01-22 23:31:16 +0100

    EJ REL4

---
+++ RELACIONES/REL4_VeraGuillermo/Ejercicio_1/Proyecto/Ejercicio_1/Assets/Scripts/StickForce.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.GetComponent<MeshFilter>().sharedMesh.name.Equals("Cylinder"))
+		{
+			gameObject.GetComponent<Rigidbody>().AddTorque(Vector3.left, ForceMode.Impulse);
+		}
+	}
+}
