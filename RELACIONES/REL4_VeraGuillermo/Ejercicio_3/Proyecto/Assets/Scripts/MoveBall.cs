ab185e5f3924086c2afdeb410d8e429c8c8fd15f
Guillermo Vera <vfguille@gmail.com>
2020-02-04 00:21:12 +0100

    EJ3 REL3

---
+++ RELACIONES/REL4_VeraGuillermo/Ejercicio_3/Proyecto/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+
+	public float _vel = 150;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void FixedUpdate()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			GetComponent<Rigidbody>().AddForce(Vector3.forward * _vel, ForceMode.Acceleration);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			GetComponent<Rigidbody>().AddForce(Vector3.back * _vel, ForceMode.Acceleration);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			GetComponent<Rigidbody>().AddForce(Vector3.right * _vel, ForceMode.Acceleration);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			GetComponent<Rigidbody>().AddForce(Vector3.left * _vel, ForceMode.Acceleration);
+		}
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (!other.gameObject.name.Equals("Plane"))
+		{
+			other.gameObject.GetComponent<Renderer>().material.color = Color.green;
+		}
+	}
+}
