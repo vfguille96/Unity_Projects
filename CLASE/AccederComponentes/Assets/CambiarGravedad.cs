0e5cbab33f9d68fa5e224c51b7338c4202fa8398
Guillermo Vera <vfguille@gmail.com>
2020-02-07 12:41:23 +0100

    Clase 07_02

---
+++ CLASE/AccederComponentes/Assets/CambiarGravedad.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarGravedad : MonoBehaviour
+{
+    private Vector3 _rotacion;
+    private float _vel;
+    public Light[] _luces = new Light[2];
+    public List<GameObject> _objetos;
+    public Transform[] _camaras;
+
+    // Use this for initialization
+    void Start()
+    {
+        _vel = 5;
+        _rotacion = Vector3.left;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotar();
+        Caer();
+    }
+
+    private void Rotar()
+    {
+        // 1ª forma de acceder a los componentes.
+        GetComponent<Transform>().Rotate(_rotacion * _vel);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _vel++;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            _vel--;
+        }
+    }
+
+    private void Caer()
+    {
+        // 2ª forma de acceder a los componentes.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ((Rigidbody) GetComponent(typeof(Rigidbody))).useGravity = true;
+            _vel = 0;
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
---
+++ CLASE/Elementos GUI/Assets/ControlGUI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlGUI : MonoBehaviour
+{
+    public GUIStyle _GuiStyle;
+    private int x = 10;
+    private int y = 10;
+    private int ancho = 200;
+    private int alto = 200;
+    public string texto = "CROQUETAS DE JAMÓN";
+    private bool toglleBol;
+    private string[] _botones = {"1", "2", "3"};
+    private int btPulsado;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    private void OnGUI()
+    {
+        Rect _area = new Rect(x, y, ancho, alto);
+        GUI.color = Color.green;
+
+        // Mostrar un label.
+        GUI.Label(_area, "BOCADILLO DE CALAMARES", _GuiStyle);
+
+
+        Rect _areaBt = new Rect(x + 20, y + 20, ancho, alto);
+
+        // Mostrar un botón
+        if (GUI.Button(_area, texto))
+        {
+            Debug.Log("BOTÓN PULSADO");
+        }
+
+        Rect _areaBtt = new Rect(x + 40, y + 40, ancho, alto);
+        GUI.Button(_areaBtt, new GUIContent("Boton 2", "Mensaje del tooltip"));
+        GUI.Label(_areaBtt, GUI.tooltip);
+
+        Rect _areaBttt = new Rect(x + 80, y + 80, ancho, alto);
+        texto = GUI.TextField(_areaBttt, texto, 20);
+
+        Rect _areaBtttt = new Rect(x + 100, y + 100, ancho, alto);
+        toglleBol = GUI.Toggle(_areaBtttt, toglleBol, "Illo soy un tuggle");
+        GameObject.Find("Directional Light").GetComponent<Light>().enabled = toglleBol;
+        
+        Rect _ultRect = new Rect(x + 150, y + 150, ancho, alto);
+        btPulsado = GUI.Toolbar(_ultRect, btPulsado, _botones);
+        Debug.Log("Botón pulsado: " + btPulsado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
