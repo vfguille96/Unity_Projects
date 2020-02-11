3fd7c57bbed92dfe13b34762c46c2c1b6b1a6ec6
Guillermo Vera <vfguille@gmail.com>
2020-02-11 12:36:59 +0100

    animación arreglada

--- RELACIONES/REL5_VeraGuillermo/UnityStrike 1.6/Assets/Scripts/Disparo.cs
+++ RELACIONES/REL5_VeraGuillermo/UnityStrike 1.6/Assets/Scripts/Disparo.cs
@@ -4,27 +4,34 @@ using UnityEngine;
 
 public class Disparo : MonoBehaviour
 {
-	private GameObject _gameObject;
+    private GameObject _gameObject;
 
-	// Use this for initialization
-	void Start ()
-	{
-		_gameObject = GameObject.Find("w_pist_deagle");
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		StartCoroutine(DisparoEagle());
-	}
+    // Use this for initialization
+    void Start()
+    {
+        _gameObject = GameObject.Find("w_pist_deagle");
+    }
 
-	private IEnumerator DisparoEagle()
-	{
-		if (Input.GetMouseButtonDown(0))
-		{
-			_gameObject.GetComponent<AudioSource>().Play();
-			_gameObject.GetComponent<Animation>().Play();
-		}
-		yield return new WaitForSecondsRealtime(0.5F);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(DisparoEagle());
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            _gameObject.GetComponent<Animator>().enabled = true;
+            _gameObject.GetComponent<Animator>().Play("Eagle2", 1, 1);
+        }*/
+    }
+
+    private IEnumerator DisparoEagle()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _gameObject.GetComponent<AudioSource>().Play();
+            _gameObject.GetComponent<Animator>().enabled = true;
+            _gameObject.GetComponent<Animator>().Play("Eagle2", 0, 0.25F);
+        }
+
+        yield return null;
+    }
 }
