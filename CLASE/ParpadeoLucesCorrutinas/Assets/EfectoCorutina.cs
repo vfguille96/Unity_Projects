58eac86179b0ea0aff1b8635c39de64703c28c79
Guillermo Vera <vfguille@gmail.com>
2020-02-21 12:34:57 +0100

    Clase_21_02

---
+++ CLASE/ParpadeoLucesCorrutinas/Assets/EfectoCorutina.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EfectoCorutina : MonoBehaviour
+{
+    public Light _Light;
+    
+
+    public void InicarParpadeo()
+    {
+        StartCoroutine(Parpadea());
+    }
+    
+    public void DetenerParpadeo()
+    {
+        StopCoroutine(Parpadea());
+    }
+    
+    
+
+    private IEnumerator Parpadea()
+    {
+        int nVeces = 15;
+
+        while (true)
+        {
+            for (int i = 0; i < nVeces; i++)
+            {
+                _Light.enabled = !_Light.enabled;
+                yield return new WaitForSeconds(Random.Range(0.1F, 0.6F));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
